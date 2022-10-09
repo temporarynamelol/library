4a90c3ecbff2961e3c0f56743c24e03695d056a5
David <David-williams-1996@outlook.com>
2022-10-09 13:27:04 +1100

    Add properties to constructor and initialise book objects

--- library.js
+++ library.js
@@ -1,11 +1,20 @@
 let myLibrary = [];
 
 // Book object constructor
-function Book() {
-
+function Book(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
 }
 
 //Adds book object to library array
-function addBook() {
-
+function addBook(title, author, pages) {
+    let book = new Book(title, author, pages);
+    myLibrary.push(book);
 }
+
+let title = prompt("Enter a title");
+let author = prompt("Enter a author");
+let pages = prompt("Enter a pages");
+
+addBook(title, author, pages);
