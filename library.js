e4c21b04618d3d2cc8833f21c7bdadada8143a18
David <David-williams-1996@outlook.com>
2022-10-24 17:58:33 +1100

    Create form div when add book clicked

--- library.js
+++ library.js
@@ -13,10 +13,15 @@ function addBook(title, author, pages) {
     myLibrary.push(book);
 } 
 
+//add book button
 let newBook = document.getElementById("newBook");
 
+//on click, query form div from html, add three new inputs for title, author and pages + new submit button
 newBook.addEventListener("click", () => {
-    let form = document.getElementById("form");
+    let body = document.querySelector("body");
+
+    let form = document.createElement("div");
+    form.setAttribute("id", "form");
 
     let title = document.createElement("input");
     title.setAttribute("id", "title");
@@ -29,23 +34,25 @@ newBook.addEventListener("click", () => {
 
     let submitButton = document.createElement("button");
     submitButton.setAttribute("id", "submitButton");
+    
+    body.appendChild(form);
 
     form.appendChild(title);
     form.appendChild(author);
     form.appendChild(pages);
     form.appendChild(submitButton);
-
+    //when submit button is clicked, call the addBook() function with the new values
     submitButton.addEventListener('click', () => {
         let title = document.getElementById('title');
         let author = document.getElementById('author');
         let pages = document.getElementById('pages');
     
         addBook(title.value, author.value, pages.value);
-    
-        title.value = author.value = pages.value = '';
-        
-        form.innerHTML = null;
-
+        //reset the values to null
+        title.value = author.value = pages.value = null;
+        //remove the new book form
+        body.removeChild(form);
+        //call populateCards() to add the new card to the page
         populateCards();
     
     });
@@ -57,7 +64,7 @@ newBook.addEventListener("click", () => {
 const bookstore = document.getElementById("bookStore");
 
 function populateCards () {
-
+    //grab the most recently added object from myLibrary[] array
     const obj = myLibrary[myLibrary.length -1];
 
     const card = document.createElement("div");
